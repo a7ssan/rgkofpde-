3021dcee8d46e3c1ce454e38cd5f31b8626e8b09
a7ssan <44026213+a7ssan@users.noreply.github.com>
2018-12-30 23:43:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const developers = ['463698464845594628','523907230068899860'];
+
 
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
@@ -17,7 +17,7 @@ client.user.setActivity(argresult , {type:'LISTENING'});
     message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
 if (message.content.startsWith('Rst')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/iitxbro");
+  client.user.setGame(argresult, "https://www.twitch.tv/ii7m0dy");
   client.user.setStatus("dnd")
    message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
@@ -26,21 +26,26 @@ client.user.setActivity(argresult , {type:'PLAYING'});
   client.user.setStatus("idle")
     message.channel.send(` ** Done. :white_check_mark: ** `)
 
-}
-  client.on('message', message => {//iiHouSSaM#9494
-        if (!developers.includes(message.author.id)) return;//iiHouSSaM#9494
-  if (message.content === 'î') {//iiHouSSaM#9494
-
-  let channel = client.channels.get('523907230068899860');//iiHouSSaM#9494
-
-
-  channel.join()//iiHouSSaM#9494
-  .then(connection => console.log(`????`))//iiHouSSaM#9494
-  .catch(console.error);//iiHouSSaM#9494
-  }
-});
-          
-});
-
+  client.on('message', message => {
+if(message.author.id !== '463698464845594628') return;
+    if (message.content.startsWith('عع')) {
+        message.channel.send(`  و ععليككم ألسلآآم و رححمة الله تعألى و بركآآتته · `)
+        message.delete()
+    }
+    });
+client.on('message', message => {
+if(message.author.id !== '463698464845594628') return;
+    if (message.content.startsWith('حح')) {
+        message.channel.send(`بخخير نححمد آلله `)
+        message.delete()
+    }
+    });
+client.on('message', message => {
+if(message.author.id !== '463698464845594628') return;
+    if (message.content.startsWith('هه')) {
+        message.channel.send(`ههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههههـــ:Clapping_: 😂`)
+        message.delete()
+    }
+    });
 client.login(process.env.BOT_TOKEN);
 
